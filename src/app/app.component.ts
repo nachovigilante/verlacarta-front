06744a40680c7c8e5e0617d8ed23031b2b2ae2c7
nachovigilante <nachovigilante@gmail.com>
2024-10-23 00:05:59 -0300

    fix(restaurants): visual updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RestaurantsListComponent } from "./restaurants-list/restaurants-list.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RestaurantsListComponent],
+    imports: [RouterOutlet, RestaurantsListComponent, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
