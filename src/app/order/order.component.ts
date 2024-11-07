c6139b38302911ac05fc10d6ca1af9e8430bb0ea
nachovigilante <nachovigilante@gmail.com>
2024-11-07 20:07:36 -0300

    feat: added searchbar to orders page

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -13,9 +13,32 @@ export class OrderComponent {
     constructor(private orderService: OrdersService) {}
 
     orders: Order[] = [];
+    filteredOrders: Order[] = [];
+    searchOrders = (e: SubmitEvent) => {
+        e.preventDefault();
+
+        const form = e.target! as HTMLFormElement;
+        const searchValue = parseInt(
+            (form.elements[0] as HTMLInputElement).value,
+        );
+
+        if (!searchValue) {
+            this.filteredOrders = this.orders;
+            return;
+        }
+
+        this.filteredOrders = this.orders.filter(
+            (order) => order.number === searchValue,
+        );
+
+        console.log(this.filteredOrders);
+    };
 
     fetchOrders() {
-        this.orderService.getOrders().then((data) => (this.orders = data));
+        this.orderService.getOrders().then((data) => {
+            this.orders = data;
+            this.filteredOrders = data;
+        });
     }
 
     ngOnInit() {
